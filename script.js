bead76ca67bed9eb60fc6edd44fab77e5ff12804
Moses <hiswordmakehim@gmail.com>
2023-04-30 23:31:45 +0100

    adding project

---
+++ script.js
@@ -0,0 +1,12 @@
+let input1=document.getElementsByClassName("input3")[0];
+let input2=document.getElementsByClassName("input3")[1];
+
+input1.addEventListener("click",function(){
+	if(input1===input1){
+		input1.style.boxShadow="0 0 0 2px #e7f3ff";
+	}
+})
+
+input2.addEventListener("click",function(){
+	input2.style.boxShadow="0 0 0 2px #e7f3ff"
+})
